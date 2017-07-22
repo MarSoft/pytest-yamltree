12418dd8a152f3a197fd78c22a78a0e294933d7e
Semyon Maryasin <simeon@maryasin.name>
2017-07-22 23:32:44 +0300

    Fix yamltree class check

--- tests/test_yamltree.py
+++ tests/test_yamltree.py
@@ -3,11 +3,12 @@
 import pytest
 
 def test_yamltree_class(tmpdir):
-    # at this point py.path.local should already be monkey-patched by conftest
-    assert hasattr(tmpdir, 'yaml_check')
+    from pytest_yamltree import YamlTree
+
+    tmp = tmpdir.strpath
 
     # for now this is an empty directory - this check should pass
-    assert tmpdir.yaml_check('{}')
+    YamlTree(tmp, '{}')
 
     # let's create some data
     struct = r'''
@@ -22,14 +23,23 @@ def test_yamltree_class(tmpdir):
             subnested:
                 hello: world
     '''
-    tmpdir.yaml_create(struct)
+    YamlTree(tmp, struct).create()
 
     # check individual parts
-    assert tmpdir.join('data.txt').yaml_check('this is just a file')
+    assert YamlTree.check_from(
+        tmpdir.join('data.txt').strpath,
+        'this is just a file',
+    )
     # and check that yaml really works
-    assert tmpdir.join('data.txt').yaml_check('"this is just a file"')
+    assert YamlTree.check_from(
+        tmpdir.join('data.txt').strpath,
+        '"this is just a file"',
+    )
     # also for empty dirs
-    assert tmpdir.join('emptydir').yaml_check('{}')
+    assert YamlTree.check_from(
+        tmpdir.join('emptydir').strpath,
+        '{}',
+    )
 
     # check something "traditionally"
     assert tmpdir.join('Keys', 'f7').read() == 'Remove!'
@@ -40,26 +50,26 @@ def test_yamltree_class(tmpdir):
     assert tmpdir.join('nested', 'subnested', 'hello').read() == 'world'
 
     # and finally check that it is accepted by yaml_check as well
-    assert tmpdir.yaml_check(struct)
+    assert YamlTree.check_from(tmp, struct)
 
     # now we want to make sure that check doesn't allow extra files...
     tmpdir.join('something.txt').write('huh')
     with pytest.raises(AssertionError):
-        tmpdir.yaml_check(struct)
+        YamlTree.check_from(tmp, struct)
 
     # ...that file contents are checked...
     tmpdir.join('something.txt').remove()
     tmpdir.join('data.txt').write('another data!!11')
     with pytest.raises(AssertionError):
-        tmpdir.yaml_check(struct)
+        YamlTree.check_from(tmp, struct)
 
     # ...that missing files are not allowed...
     tmpdir.join('data.txt').remove()
     with pytest.raises(AssertionError):
-        tmpdir.yaml_check(struct)
+        YamlTree.check_from(tmp, struct)
 
     # ...and that file is differentiated from directory.
     with pytest.raises(AssertionError):
-        tmpdir.yaml_check('I thought it is a file')
+        YamlTree.check_from(tmp, 'I thought it is a file')
     with pytest.raises(AssertionError):
-        tmpdir.yaml_check('nested: I thought it is a file')
+        YamlTree.check_from(tmp, 'nested: I thought it is a file')
