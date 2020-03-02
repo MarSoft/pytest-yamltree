326d35afdbcaee1a14f749baf81ab342dc88fc2a
Semyon Maryasin <simeon@maryasin.name>
2020-03-03 01:13:37 +0300

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-yamltree',
-    version='0.1.1',
+    version='0.1.2',
     author='Semyon Maryasin',
     author_email='simeon@maryasin.name',
     maintainer='Semyon Maryasin',
